f34407d1f44122b41a70d0f532a98fccfe56d997
hex2077 <justlikemaki@qq.com>
2025-09-28 21:37:15 +0800

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,7 +37,7 @@ export const auth = betterAuth({
 		}
 	},
   secret: process.env.BETTER_AUTH_SECRET!,
-  baseURL: process.env.BETTER_AUTH_URL!,
+  baseURL: process.env.BETTER_AUTH_URL!, 
   telemetry: {
     enabled: false,
   },
