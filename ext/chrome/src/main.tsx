469938daa3dd707c3351e3e77d4156a277e66c32
hex2077 <justlikemaki@qq.com>
2025-10-29 14:15:29 +0800

    remove github action

--- ext/chrome/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import SidePanelApp from './components/SidePanelApp';
-import './assets/index.css';
-
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<SidePanelApp />);
--- ext/desktop/postcss.config.js
+++
@@ -1,7 +0,0 @@
-/**
- * PostCSS 配置文件
- * Desktop 应用不使用 Tailwind CSS，使用空配置
- */
-module.exports = {
-  plugins: {}
-}
